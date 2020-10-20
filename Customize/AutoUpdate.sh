3643b9d2ab7af924877081cf7d25f02823152dd9
y403321959 <35832654+y403321959@users.noreply.github.com>
2020-10-20 11:26:58 +0800

    Update AutoUpdate.sh

--- Customize/AutoUpdate.sh
+++ Customize/AutoUpdate.sh
@@ -5,7 +5,7 @@
 
 Version=V3.7
 DEFAULT_DEVICE=d-team_newifi-d2
-Github=https://github.com/Hyy2001X/AutoBuild-Actions
+Github=https://github.com/y403321959/AutoBuild-Actions
 
 TIME() {
 	echo -ne "\n[$(date "+%H:%M:%S")] "
